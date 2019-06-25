5b2edc6b7dfa7fb10448591a80a955d89d4cef1a
Adrian Wix <adrianwix18@gmail.com>
2019-06-25 16:24:22 +0200

    Message Board model

---
+++ models/Thread.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const threadSchema = new Schema({
+	board: {
+		type: String,
+		required: true
+	},
+	text: {
+		type: String,
+		required: true
+	},
+	created_on: {
+		type: Date,
+		default: Date.now
+	},
+	bumped_on: {
+		type: Date,
+		default: Date.now
+	},
+	reported: {
+		type: Boolean,
+		default: false,
+		required: true
+	},
+	delete_password: {
+		type: String,
+		required: true
+	},
+	replies: [
+		{
+			text: {
+				type: String,
+				required: true
+			},
+			delete_password: {
+				type: String,
+				required: true
+			},
+			created_on: {
+				type: Date,
+				default: Date.now
+			},
+			reported: {
+				type: Boolean,
+				default: false
+			}
+		}
+	]
+});
+
+const Thread = mongoose.model('Thread', threadSchema);
+
+module.exports = Thread;
